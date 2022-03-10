fb5336245deb1b5093f3319e07bdb632e4b344ba
rodrigoSouza11 <rodrigo.souza@4innovation.co>
2022-03-09 21:46:34 -0300

    funcoes estao dessincronizadas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,59 +18,59 @@ export class HomePage {
   positionSubscription: any;
   routeGroup: any
   routeOptions: any
-  
-  marker : any;
+
+  marker: any;
 
   constructor() {
-    
-  }  
+
+  }
 
   ionViewDidEnter() {
     this.a()
     //this.e()    
-}
+  }
 
-async mapStart(){
-  console.log('load')   
-    
+  async mapStart() {
+    console.log('load')
+
     console.log(this.center, 'cccc')
-    
+
     this.center
     this.destino = { lng: -46.825855, lat: -23.009912 }
-    
-    
+
+
     this.map = tt.map({
       key: "cG20UJo9g3TCvDYuAAecV6F6F2v6BSBe",
       container: "map",
       center: this.center,
       zoom: 16,
-      bearing: 150,  //=> regula a totacao         
-    });   
+      bearing: 180,  //=> regula a totacao         
+    });
 
     this.map.addControl(new tt.FullscreenControl());
     this.map.addControl(new tt.NavigationControl());
-    
+
     this.routeOptions = {
       key: 'cG20UJo9g3TCvDYuAAecV6F6F2v6BSBe',
-      locations: [this.center, this.destino   ],
+      locations: [this.center, this.destino],
       travelMode: 'truck'
     }
-    
-    this.pointCentralMark();   
- 
-}
+
+    this.pointCentralMark();
+
+  }
 
 
-  
-  calculateRoute(){ 
+
+  calculateRoute() {
     ttapi.services.calculateRoute(this.routeOptions).then((routeData) => {
-      console.log(routeData.toGeoJson()) 
-this.drawRoute(routeData.toGeoJson())         
+      console.log(routeData.toGeoJson())
+      this.drawRoute(routeData.toGeoJson())
     })
   }
 
-  drawRoute (json) {
-    console.log('www' , json)
+  drawRoute(json) {
+    console.log('www', json)
     this.routeGroup = {}
     this.routeGroup.outlinelayer = {
       id: 'routeOutline',
@@ -101,67 +101,81 @@ this.drawRoute(routeData.toGeoJson())
   }
 
 
-async a(){
-  const posicaoAtual: Position = await Geolocation.getCurrentPosition();   
-  this.center = { lng: posicaoAtual.coords.longitude, lat: posicaoAtual.coords.latitude }
-  this.y = posicaoAtual.coords.latitude
+  async a() {
+    const posicaoAtual: Position = await Geolocation.getCurrentPosition();
+    this.center = { lng: posicaoAtual.coords.longitude, lat: posicaoAtual.coords.latitude }
+    this.y = posicaoAtual.coords.latitude
 
-  console.log(posicaoAtual,'GPS endereco')
+    console.log(posicaoAtual, 'GPS endereco')
 
-  this.mapStart() 
+    this.mapStart()
 
-}
-
- 
-
-async getLocation() {
-  //const posicaoAtual: Position = await Geolocation.getCurrentPosition();
-  //console.log('a', posicaoAtual )  
-}
-
-async pointCentralMark(){
-  const posicaoAtual: Position = await Geolocation.getCurrentPosition() 
-  this.marker = new tt.Marker({color: 'red'}).setLngLat(this.center).addTo(this.map); 
-   
-  
-  //this.e()
-  //this.aa()
-}
-
-
-
-async e(){
-  
-  let options =  {
-    enableHighAccuracy: true,
-    timeout: 10000,    
-    maximumAge: 0
   }
 
-  
 
-  let n = await Geolocation.watchPosition(options, (pos) => {
-    if(pos){
-    this.marker.remove()     
-      
-    this.center = { lng: pos.coords.longitude, lat: pos.coords.latitude }
-    
-    let marker = new tt.Marker({color: 'green'}).setLngLat(this.center).addTo(this.map);
-    
-    let marker2 = new tt.Marker({color: 'black'}).setLngLat(this.destino).addTo(this.map);
-    
-    console.log(pos,this.center, 'qqqq')
-    this.calculateRoute()
-    return 
+
+  async getLocation() {
+    //const posicaoAtual: Position = await Geolocation.getCurrentPosition();
+    //console.log('a', posicaoAtual )  
+  }
+
+  async pointCentralMark() {
+    const posicaoAtual: Position = await Geolocation.getCurrentPosition()
+    this.marker = new tt.Marker({ color: 'red' }).setLngLat(this.center).addTo(this.map);
+
+
+    //this.e()
+    //this.aa()
+  }
+
+
+
+  async e() { 
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0
     }
-    
-  })  
 
-  
-}}
-
+    let n = await Geolocation.watchPosition(options, (pos) => {
+      console.log(pos, 'iiiii')
+      this.marker.remove() 
+      console.log('1')
+      this.s(pos)     
+    })   
     
-  
+  }
+
+  async s(pos) {
+    await this.marker.remove()    
+    this.t(pos)
+    console.log('2')
+}
+
+async t(pos){
+  console.log('3')
+  await this.marker.remove() 
+  console.log('4')
+      this.center = { lng: pos.coords.longitude, lat: pos.coords.latitude }
+
+      let marker = new tt.Marker({ color: 'green', rotationAlignment: 'auto' }).setLngLat(this.center).addTo(this.map);
+
+      let marker2 = new tt.Marker({ color: 'black' }).setLngLat(this.destino).addTo(this.map);
+
+      console.log(pos, this.center, 'qqqq')
+      this.calculateRoute()
+      return
+}
+
+
+
+}
+
+
+
+
+
+
 
 /*
 watchPosition = async () => {
